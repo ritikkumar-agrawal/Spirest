2a4a77574f8a8a69cc348e4a19a16c1d62a0e8b5
Ritik Agarwal <121ec0259@nitrkl.ac.in>
2024-11-02 12:13:31 +0530

    removed

--- src/utils/db.ts
+++
@@ -1,4 +0,0 @@
-// import { MongoClient } from 'mongodb';
-
-// const uri = 'mongodb+srv://ritikagrawal3268:gNQiCiZvMwPKrAkL@students.kq4wx.mongodb.net/?retryWrites=true&w=majority&appName=Students'; // Replace with your connection string
-
